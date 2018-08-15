dad8502dee0c6a46a256e3cc6a84d1531f501fea
Maximilian Falkenstein <maximilian.falkenstein@vseth.ethz.ch>
2018-08-15 13:25:11 +0200

    Switch to new build strategy

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:stretch
+RUN mkdir /build && apt update && apt install -y rsync
+COPY cni-plugins /build/cni-plugins
+COPY etcd /build/etcd
+COPY kubernetes /build/kubernetes
+COPY build.sh /build
+# ETCD build relies on this...
+COPY .git /build/.git
+RUN cd /build && ./build.sh && tar -cvf /payload.tar artifacts
+
+FROM busybox
+COPY --from=0 /payload.tar /
+CMD tar -xvf /payload.tar -C /target
--- build.sh
+++ build.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+mkdir artifacts
+mkdir artifacts/linux
+mkdir artifacts/windows
+
 echo "Building microkube dependencies"
 echo "###############################"
 echo "########## CNI Plugins ########"
 echo "###############################"
 cd cni-plugins
 ./build.sh
+cp bin/* ../artifacts/linux
+# At the moment, no CNI on Windows
+# rm bin/*
+# GOOS=windows GOARCH=amd64 ./build.sh
+# cp bin/* ../artifacts/windows
 cd ..
 echo "###############################"
 echo "############# ETCD ############"
@@ -15,17 +24,22 @@ echo "###############################"
 cd etcd
 git checkout build
 ./build
+cp bin/* ../artifacts/linux
+rm bin/*
+GOOS=windows GOARCH=amd64 ./build
+cp bin/* ../artifacts/windows
 cd ..
 echo "###############################"
 echo "########## Kubernetes #########"
 echo "###############################"
 cd kubernetes
 make
-cd ..
+cp _output/local/bin/linux/amd64/hyperkube ../artifacts/linux/
+KUBE_BUILD_PLATFORMS=windows/amd64 make
+cp _output/local/bin/windows/amd64/hyperkube.exe ../artifacts/windows/
 
-mkdir artifacts
-cp etcd/bin/* artifacts/
-cp cni-plugins/bin/* artifacts/
-cp kubernetes/_output/local/bin/linux/amd64/hyperkube artifacts/
-cd artifacts
+cd ../artifacts
+cd linux
+strip *
+cd ../windows
 strip *
